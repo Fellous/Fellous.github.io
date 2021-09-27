115d7e79bcec6f6135c9aaa02af26bcef35d9609
Fellous <fellousdavid95200@gmail.com>
2021-09-27 11:11:21 +0200

    jeu
    insertion du jeu

---
+++ ProjetJSBufeteau/ProjetJS.V2/JS/MesFonctions.js
@@ -0,0 +1,14 @@
+// Rideau
+$(
+    function()
+    {
+        $('#div4').click(function(){
+            $('#div4').animate({left:'-1500px'},2000,function(){
+                $('#div5').animate({left:'1600px'},2000,function(){  
+                });
+            });
+        });
+                    
+    }  
+);   
+// Memory 
